08f43dfaebf7cccd1c8e52531c4292ef2ad68951
tron-stack <kikaniparth@gmail.com>
2024-03-04 10:03:07 -0600

    .

--- src/pages/SelectDocs.jsx
+++ src/pages/SelectDocs.jsx
@@ -1,11 +1,22 @@
+import { useDispatch } from "react-redux";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 function SelectDocs(){
 
     const [allDocs, setAllDocs] = useState([]);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        dispatch();
+    })
+
 
     return(<>
     <Navbar/>
+    {/* <DataGrid/> */}
     </>)
 }
